c8f1d3976c18856df2f2ce106416d3ab8be8afc0
Bharadartist <ase.bharath@gmail.com>
2019-09-19 20:49:29 +0530

    Added Image target UI functionality

---
+++ Factory AR/Assets/Scripts/UI_Canvas.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class UI_Canvas : MonoBehaviour
+{
+    #region variables
+    [Header("System Events")]
+    public UnityEvent onSwitchedScreen = new UnityEvent();
+    private Component[] appScreens = new Component[0];
+    private UI_Screen currentScreen;
+    public UI_Screen CurrentScreen { get { return currentScreen; } }
+    private UI_Screen previousScreen;
+    public UI_Screen PreviousScreen { get { return previousScreen; } }
+    #endregion
+
+    #region main methods
+    // Start is called before the first frame update
+    void Start()
+    {
+        appScreens = GetComponentsInChildren<UI_Screen>(true);
+        if (appScreens.Length >0)
+        {
+            SwitchScreen(appScreens[0].GetComponent<UI_Screen>());
+        }
+    }
+    #endregion
+
+    #region helper methods
+    public void SwitchScreen(UI_Screen activeScreen)
+    {
+        if (activeScreen)
+        {
+            if (currentScreen)
+            {
+                previousScreen = currentScreen;
+                previousScreen.gameObject.SetActive(false);
+            }
+        }
+        currentScreen = activeScreen;
+        currentScreen.gameObject.SetActive(true);
+        if(onSwitchedScreen != null)
+        {
+            onSwitchedScreen.Invoke();
+        }
+    }
+
+    public void GoToPreviousScreen()
+    {
+        if (previousScreen)
+        {
+            SwitchScreen(previousScreen);
+        }        
+    }
+    #endregion
+
+}
---
+++ Factory AR/Assets/Scripts/UI_Screen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class UI_Screen : MonoBehaviour
+{
+    #region variables
+
+    //public UnityEvent onScreenStart
+
+    #endregion
+
+    #region main methods
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    #endregion
+
+    #region helper methods
+
+    #endregion
+}
